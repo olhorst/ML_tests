c243092f415fa4c8ff3a575ea49b27edaf4109c5
Benjamin Berta <berta@campus.tu-berlin.de>
2020-05-24 22:03:37 +0200

    initial commit for the 2. problem set

---
+++ problem_set2/ps2_implementation.py
@@ -0,0 +1,125 @@
+""" ps2_implementation.py
+
+PUT YOUR NAME HERE:
+<FIRST_NAME><LAST_NAME>
+
+
+Write the functions
+- kmeans
+- kmeans_agglo
+- agglo_dendro
+- norm_pdf
+- em_gmm
+- plot_gmm_solution
+
+(c) Felix Brockherde, TU Berlin, 2013
+    Translated to Python from Paul Buenau's Matlab scripts
+"""
+
+from __future__ import division  # always use float division
+import numpy as np
+from scipy.spatial.distance import cdist  # fast distance matrices
+from scipy.cluster.hierarchy import dendrogram  # you can use this
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D  # for when you create your own dendrogram
+
+def kmeans(X, k, max_iter=100):
+    """ Performs k-means clustering
+
+    Input:
+    X: (d x n) data matrix with each datapoint in one column
+    k: number of clusters
+    max_iter: maximum number of iterations
+
+    Output:
+    mu: (d x k) matrix with each cluster center in one column
+    r: assignment vector
+    """
+
+    pass
+
+
+def kmeans_agglo(X, r):
+    """ Performs agglomerative clustering with k-means criterion
+
+    Input:
+    X: (d x n) data matrix with each datapoint in one column
+    r: assignment vector
+
+    Output:
+    R: (k-1) x n matrix that contains cluster memberships before each step
+    kmloss: vector with loss after each step
+    mergeidx: (k-1) x 2 matrix that contains merge idx for each step
+    """
+
+    def kmeans_crit(X, r):
+        """ Computes k-means criterion
+
+        Input: 
+        X: (d x n) data matrix with each datapoint in one column
+        r: assignment vector
+
+        Output:
+        value: scalar for sum of euclidean distances to cluster centers
+        """
+
+        pass
+    
+    pass
+
+
+
+def agglo_dendro(kmloss, mergeidx):
+    """ Plots dendrogram for agglomerative clustering
+
+    Input:
+    kmloss: vector with loss after each step
+    mergeidx: (k-1) x 2 matrix that contains merge idx for each step
+    """
+
+    pass
+
+
+def norm_pdf(X, mu, C):
+    """ Computes probability density function for multivariate gaussian
+
+    Input:
+    X: (d x n) data matrix with each datapoint in one column
+    mu: vector for center
+    C: covariance matrix
+
+    Output:
+    pdf value for each data point
+    """
+
+    pass
+
+
+def em_gmm(X, k, max_iter=100, init_kmeans=False, eps=1e-3):
+    """ Implements EM for Gaussian Mixture Models
+
+    Input:
+    X: (d x n) data matrix with each datapoint in one column
+    k: number of clusters
+    max_iter: maximum number of iterations
+    init_kmeans: whether kmeans should be used for initialisation
+    eps: when log likelihood difference is smaller than eps, terminate loop
+
+    Output:
+    pi: 1 x k matrix of priors
+    mu: (d x k) matrix with each cluster center in one column
+    sigma: list of d x d covariance matrices
+    """
+
+    pass
+
+def plot_gmm_solution(X, mu, sigma):
+    """ Plots covariance ellipses for GMM
+
+    Input:
+    X: (d x n) data matrix with each datapoint in one column
+    mu: (d x k) matrix with each cluster center in one column
+    sigma: list of d x d covariance matrices
+    """
+
+    pass
